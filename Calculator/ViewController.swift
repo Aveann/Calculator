02290413fc5fde28a2bb92ee10afd20e652bfb4c
Naëva Arnould <naeva.ar@gmail.com>
2017-04-20 14:48:13 +0200

    First commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Naëva Arnould on 20/04/2017.
+//  Copyright © 2017 Naëva Arnould. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func touchDigit (_ sender : UIButton){
+    
+    }
+
+    
+}
+
