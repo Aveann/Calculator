4cc6d98cacb3c092daa441bd2707a12c7b55b99c
Naëva Arnould <naeva.ar@gmail.com>
2017-04-21 08:51:46 +0200

    Fonctionnal calculator

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,10 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBAction func touchDigit (_ sender : UIButton){
+    @IBOutlet weak var display: UILabel!
     
+    var userIsTyping = false
+    
+    @IBAction func touchDigit (_ sender : UIButton){
+        let digit = sender.currentTitle!
+        
+        if userIsTyping{
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+            userIsTyping = true
+        }
     }
-
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    private var brain = CalculatorBrain()
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        
+        if userIsTyping {
+            brain.setOperand(displayValue)
+            userIsTyping = false
+        }
+        
+        userIsTyping = false
+        
+        if let mathSymbol = sender.currentTitle {
+            brain.performOperation(mathSymbol)
+        }
+        
+        if let result = brain.result {
+            displayValue = result
+        }
+    }
+    
+    
     
 }
 
---
+++ README.md
@@ -0,0 +1,2 @@
+The calculator project used in the Stanford iOS 10 class.
+I'll update it as I go through the course.
